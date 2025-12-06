404b00fa59b139c8951d29c4fb3932d9eda3f022
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-12-06 23:57:36 +0530

    ds

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import connectTODB from "./database/index.js"
 import axios from "axios"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import morgan from "morgan"
+import authRouter from "./router/authRouter.js"
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 dotenv.config()
 const app = express()
 
@@ -11,16 +16,24 @@ const port = process.env.PORT
 await connectTODB()
 // middleware
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(morgan("dev"))
 
 
 // router
+app.use("/api/auth", authRouter)
+
+
 app.get("/", (req, res) => {
 
 
     res.send("Api is working ")
 
 })
-
+// Error handling middleware
+app.use(notFound)
+app.use(errorHandler)
 // for reseting the uptime
 const url = "https://assignment-task-1.onrender.com"
 const interval = 30000
